be1ba495f403fa4a5880b57726b00a8a514ccab1
Elena_Syurtukova <evasineva@bk.ru>
2025-02-09 01:13:59 +0900

    flake8, mypy

--- src/masks.py
+++ src/masks.py
@@ -1,18 +1,12 @@
 def get_mask_card_number(card_number: int) -> str:
     """Возвращает маску номера карты: видны первые 6 и последние 4 цифры номера"""
-    mask_card_number = (
-        str(card_number)[0:4]
-        + " "
-        + str(card_number)[4:6]
-        + "** **** "
-        + str(card_number)[12:]
-    )
+    mask_card_number = str(card_number)[0:4] + " " + str(card_number)[4:6] + "** **** " + str(card_number)[12:]
     return mask_card_number
 
 
 def get_mask_account(account_number: int) -> str:
     """Возвращает маску номера счета в виде ** и последних 4-х цифр счета"""
-    mask_account_number = "**" + str(account_number)[len(str(account_number)) - 4 :]
+    mask_account_number = "**" + str(account_number)[len(str(account_number)) - 4:]
     return mask_account_number
 
 
--- src/widget.py
+++ src/widget.py
@@ -1,10 +1,11 @@
 from masks import get_mask_card_number
 from masks import get_mask_account
 
+
 def mask_account_card(model_number: str) -> str:
     """Функция, которая маскирует тип и номер карты или счета"""
-    if 'Счет' in model_number:
-        mask_model_number = 'Счет ' + get_mask_account(int(model_number[5:]))
+    if "Счет" in model_number:
+        mask_model_number = "Счет " + get_mask_account(int(model_number[5:]))
     else:
         mask_model_number = model_number[:-16] + get_mask_card_number(int(model_number[-16:]))
     return mask_model_number
@@ -12,5 +13,5 @@ def mask_account_card(model_number: str) -> str:
 
 def get_date(date: str) -> str:
     """Функция, которая меняет формат даты"""
-    formated_date = date[8:10]+'.'+date[5:7]+'.'+date[:4]
+    formated_date = date[8:10] + "." + date[5:7] + "." + date[:4]
     return formated_date
