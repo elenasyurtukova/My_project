58347d569a27bee95576c7e5d9179e45c996d78e
Elena_Syurtukova <evasineva@bk.ru>
2025-02-15 15:50:06 +0900

    add fnc sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 def filter_by_state(data: list, state: str = "EXECUTED") -> list:
     """Фильтрует список словарей по ключу state"""
     filter_data = []
@@ -5,6 +7,17 @@ def filter_by_state(data: list, state: str = "EXECUTED") -> list:
         if elem["state"] == state:
             filter_data.append(elem)
     return filter_data
+
+
+def sort_by_date(data: list) -> list:
+    """Сортирует список по дате"""
+    sorted_data = sorted(data, key=lambda x: datetime.strptime(x["date"],'%Y-%m-%dT%H:%M:%S.%f'), reverse=True)
+    return sorted_data
+
+# print(sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+#  {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+#  {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+#  {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
 
 
 #[{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
