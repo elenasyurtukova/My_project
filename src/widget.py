eb2634e18fd84c50546365314c01475cef7e584d
Elena_Syurtukova <evasineva@bk.ru>
2025-02-15 11:38:12 +0900

    add branches

--- src/widget.py
+++ src/widget.py
@@ -1,8 +1,9 @@
-from masks import get_mask_card_number
-from masks import get_mask_account
+from typing import Any
+
+from masks import get_mask_account, get_mask_card_number
 
 
-def mask_account_card(model_number: str) -> str:
+def mask_account_card(model_number: str) -> Any:
     """Функция, которая маскирует тип и номер карты или счета"""
     if "Счет" in model_number:
         mask_model_number = "Счет " + get_mask_account(int(model_number[5:]))
