d0974d50a206c6c0ff949be9d061d18bfd58e4d8
Elena_Syurtukova <evasineva@bk.ru>
2025-02-16 17:03:44 +0900

    flake8, mypy success

--- src/widget.py
+++ src/widget.py
@@ -1,6 +1,6 @@
 from typing import Any
 
-from masks import get_mask_account, get_mask_card_number
+from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(model_number: str) -> Any:
