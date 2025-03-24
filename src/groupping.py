d04e6766f697a60a5e085ee033f84e02d3591c29
Elena_Syurtukova <evasineva@bk.ru>
2025-03-25 03:23:20 +0900

    readme

--- src/groupping.py
+++ src/groupping.py
@@ -2,7 +2,7 @@ from collections import Counter
 
 
 def group_by_categories(list_of_trans: list, list_categories: list) -> dict:
-    # Группировка данных по категориям
+    """Группировка данных по категориям"""
     dict = {}
     for elem in list_categories:
         count = Counter(elem.lower() in trans["description"].lower() for trans in list_of_trans)
