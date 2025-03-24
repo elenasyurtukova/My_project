7470f35f0e86eeb0d5f6eb19754ab04196f143d7
Elena_Syurtukova <evasineva@bk.ru>
2025-03-25 01:36:29 +0900

    test_searching.py

--- src/searching.py
+++ src/searching.py
@@ -10,13 +10,14 @@ def search_by_string(list_of_dict:list, search_string:str = 'Перевод')->l
             filtered_list.append(trans)
     return filtered_list
 
-# if __name__ == "__main__":
-#     print(search_by_string([{'id': '650703', 'state': 'EXECUTED', 'date': '2023-09-05T11:30:32Z',
-#     'amount': '16210', 'currency_name': 'Sol', 'currency_code': 'PEN',
-#     'from': 'Счет 58803664561298323391', 'to': 'Счет 39745660563456619397',
-#     'description': 'Перевод организации'},
-#     {'id': '3598919', 'state': 'EXECUTED', 'date': '2020-12-06T23:00:58Z',
-#     'amount': '29740', 'currency_name': 'Peso', 'currency_code': 'COP',
-#     'from': 'Discover 3172601889670065', 'to': 'Discover 0720428384694643',
-#     'description': 'Перевод с карты на карту'}], 'zxcvv'))
+if __name__ == "__main__":
+ #   print(search_by_string([]))
+    print(search_by_string([{'id': '650703', 'state': 'EXECUTED', 'date': '2023-09-05T11:30:32Z',
+    'amount': '16210', 'currency_name': 'Sol', 'currency_code': 'PEN',
+    'from': 'Счет 58803664561298323391', 'to': 'Счет 39745660563456619397',
+    'description': 'Перевод организации'},
+    {'id': '3598919', 'state': 'EXECUTED', 'date': '2020-12-06T23:00:58Z',
+    'amount': '29740', 'currency_name': 'Peso', 'currency_code': 'COP',
+    'from': 'Discover 3172601889670065', 'to': 'Discover 0720428384694643',
+    'description': 'Перевод с карты на карту'}], ))
 
---
+++ tests/test_searching.py
@@ -0,0 +1,38 @@
+import pytest
+
+from src.searching import search_by_string
+
+
+@pytest.fixture()
+def data():
+    return [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364", "description": "Открытие вклада"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572", "description": "Перевод со счета на счет"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689", "description": "Перевод организации"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441", "description": "Перевод с карты на счет"}
+    ]
+
+@pytest.mark.parametrize(
+    "string, expected",
+    [
+        (
+            "вклад",
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364", "description": "Открытие вклада"},
+            ],
+        ),
+        (
+            "счет",
+            [
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572", "description": "Перевод со счета на счет"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441", "description": "Перевод с карты на счет"},
+            ],
+        )
+    ]
+)
+
+def test_search_by_string(data, string, expected):
+    assert search_by_string(data,string) == expected
+
+def test_search_by_string_empty():
+    assert search_by_string([]) == []
