6af4e8307dfe40e54fd79b2711f7b8de5a34b688
Elena_Syurtukova <evasineva@bk.ru>
2025-03-25 02:11:40 +0900

    test_groupping.py

--- src/groupping.py
+++ src/groupping.py
@@ -14,10 +14,16 @@ def group_by_categories(list_of_trans:list, list_categories:list)->dict:
 
 
 if __name__ == "__main__":
-    list_of_trans = func_read_file_csv("../data/transactions.csv")
-    list_categories = ['перевод', 'вклад', 'счет']
+  #  list_of_trans = func_read_file_csv("../data/transactions.csv")
+    list_1 = [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364", "description": "Открытие вклада"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572", "description": "Перевод со счета на счет"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689", "description": "Перевод организации"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441", "description": "Перевод с карты на счет"}
+    ]
+    list_categories = ['перевод', 'деньги']
 
-    result = group_by_categories(list_of_trans, list_categories)
+    result = group_by_categories({"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364", "description": "Открытие вклада"}, list_categories)
     print(result)
 
 
