0aeeea96aa4575c63c16387a9172ec6ff44fdb1e
Elena_Syurtukova <evasineva@bk.ru>
2025-03-24 21:07:19 +0900

    changed groupping.py

--- src/groupping.py
+++ src/groupping.py
@@ -1,7 +1,5 @@
 from collections import Counter
 
-import pandas as pd
-
 from src.file_read import func_read_file_csv
 
 
@@ -9,24 +7,18 @@ def group_by_categories(list_of_trans:list, list_categories:list)->dict:
     # Группировка данных по категориям
     dict = {}
     for elem in list_categories:
-        count = 0
-        for trans in list_of_trans:
-            if trans['description'] == elem:
-                count += 1
-        dict[elem] = count
+        count = Counter(elem.lower() in trans['description'].lower() for trans in list_of_trans)
+        dict[elem] = count[True]
     return dict
 
 
 
-# if __name__ == "__main__":
-#     list_of_trans = func_read_file_csv("../data/transactions.csv")
-#     list_categories = []
-#     for trans in list_of_trans:
-#         if trans['description'] not in list_categories:
-#             list_categories.append(trans['description'])
-#
-#     result = group_by_categories(list_of_trans, list_categories)
-#     print(result)
+if __name__ == "__main__":
+    list_of_trans = func_read_file_csv("../data/transactions.csv")
+    list_categories = ['перевод', 'вклад', 'счет']
+
+    result = group_by_categories(list_of_trans, list_categories)
+    print(result)
 
 
 
